2bb8d9e3c1efd84d423efb330987b199615f2ed8
Justin-Rudolph <86308692+Justin-Rudolph@users.noreply.github.com>
2026-02-03 16:43:25 -0500

    Updated firebase.ts config to use vite .env variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,13 +9,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCH1ZmvofzO5xeeoEuTGWoOsd3Xn9w1PJY",
-  authDomain: "suros-logic.firebaseapp.com",
-  projectId: "suros-logic",
-  storageBucket: "suros-logic.firebasestorage.app",
-  messagingSenderId: "57271164226",
-  appId: "1:57271164226:web:6100a0784c23d98f61fcdb",
-  measurementId: "G-X7X3X2CYQ4"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
